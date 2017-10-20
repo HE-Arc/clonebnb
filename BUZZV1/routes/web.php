26bd086ab2983583170efdb826684afec8f1d1fe
ombangnc <charlomb@yahoo.fr>
2017-10-20 23:53:13 +0200

    Création des routes et ajout du helper créé dans le fichier composer.json

--- BUZZV1/routes/web.php
+++ BUZZV1/routes/web.php
@@ -10,7 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Mail\ContactMessageCreated;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Page d'acceuil
+Route::get('/', [
+	'as' => 'root_path',
+	'uses'=> 'PagesController@home'
+]);
+
+//Route::name('root_path ')->get('/','PagesController@home')
+//ou
+//Route::get('/','PagesController@home')->name('root_path ')
+
+//Page about
+Route::get('/about', [
+	'as' => 'about_path',
+	'uses'=> 'PagesController@about'
+]);
+
+//Page contact
+Route::get('/contact', [
+	'as' => 'contact_path',
+	'uses'=> 'ContactsController@create'
+]);
+Route::post('/contact', [
+	'as' => 'contact_path',
+	'uses'=> 'ContactsController@store'
+]);
+
+//Page d'inscription
+Route::get('/signup', [
+	'as'=>'signup_path',
+	'uses'=>'UserSignupController@create'
+]);
