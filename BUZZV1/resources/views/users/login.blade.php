3dac393433b5c0911b0927f58714e87d6e1a7f89
ombangnc <charlomb@yahoo.fr>
2017-10-21 10:48:11 +0200

    Ajout des formulaires de connexion, incription, reset du mot de passe

---
+++ BUZZV1/resources/views/users/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+
+@section('title')
+	Connexion à Buzz!
+@stop
+
+@section('content')
+	<div class="container">
+		<div class="login-form">
+			<form class="modal-content animate" action="/action_page.php">
+    			<div class="imgcontainer">
+            <a href="{{ route('root_path') }}"><img src="{{ asset('images/logo-form.png') }}"></a>
+    			</div>
+      			<div class="container">
+      				<button class="btn btn-lg" type="submit" style="background: #4568b2 !important;" disabled="disabled">Connexion avec Facebook</button>
+      				<button class="btn btn-lg" type="submit" style="background-color: #fff !important; border: 1px solid; color: #484848" disabled="disabled">Connexion avec Google</button>
+    			</div>
+    			<span style="text-align: center">Ou</span>
+    			<div class="container">
+    				<div class="icon-in-input">
+    					<input type="text" placeholder="Adresse email" name="uname" required>
+      					<i class="fa fa-at fa-lg fa-fw" aria-hidden="true"></i>
+    				</div>
+    				<div class="icon-in-input">
+      					<input type="password" placeholder="Mot de passe" name="psw" required>
+      					<i class="fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
+    				</div>
+      				<button class="btn btn-lg" type="submit" style="background: #FF5A5F !important;">Connexion</button>
+      				<div class="min-container">
+      					<span><a href="{{ route('reset_path') }}">Mot de passe oublié ?</a></span>
+      				</div>
+    			</div>
+    			<div class="container min-container" style="background-color:#f1f1f1;">
+      				<span>Vous n'avez pas de compte ?<a href="{{ route('signup_path') }}"> Insrivez-vous</a></span>
+    			</div>
+  			</form>
+		</div>
+	</div>
+@stop
+
+@section('stylesheet')
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+@stop
---
+++ BUZZV1/resources/views/users/signup.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('title')
+  S'inscrire à Buzz!
+@stop
+
+@section('content')
+  <div class="container">
+    <div class="login-form">
+      <form class="modal-content animate" action="/action_page.php">
+          <div class="imgcontainer">
+              <a href="{{ route('root_path') }}"><img src="{{ asset('images/logo-form.png') }}"></a>
+          </div>
+            <div class="container min-container" style="background-color:#f1f1f1;">
+              <span>S'inscire avec  <a href="#">Facebook</a> ou <a href="#"> Google</a></span>
+            </div>
+          <span style="text-align: center">Ou</span>
+          <div class="container">
+            <div class="icon-in-input">
+              <input type="text" placeholder="Adresse email" name="uname" required>
+                <i class="fa fa-at fa-lg fa-fw" aria-hidden="true"></i>
+            </div>
+            <div class="icon-in-input">
+                <input type="text" placeholder="Prénom" name="psw" required>
+                <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+            </div>
+            <div class="icon-in-input">
+              <input type="text" placeholder="Nom" name="uname" required>
+                <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+            </div>
+            <div class="icon-in-input">
+                <input type="password" placeholder="Créer un mot de passe" name="psw" required>
+                <i class="fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
+            </div>
+              <button class="btn btn-lg" type="submit" style="background: #FF5A5F !important;">Inscription</button>
+          </div>
+          <div class="container min-container" style="background-color:#f1f1f1;">
+              <span>Déjà inscrit ?<a href="{{ route('login_path') }}"> Connectez-vous</a></span>
+          </div>
+        </form>
+    </div>
+  </div>
+@stop
+
+@section('stylesheet')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+@stop
