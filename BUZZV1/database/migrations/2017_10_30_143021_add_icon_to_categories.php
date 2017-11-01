9d32059f9ef0d75777b507917180084f2f342602
Thomas Lovis <thomas.lovis@gmail.com>
2017-11-01 17:05:32 +0100

    modify migration add icon

--- BUZZV1/database/migrations/2017_10_30_143021_add_icon_to_categories.php
+++ BUZZV1/database/migrations/2017_10_30_143021_add_icon_to_categories.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIconToCategories extends Migration
+class AddIconToCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
