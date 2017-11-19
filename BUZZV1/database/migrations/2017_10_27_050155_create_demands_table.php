b557b478e4b396ba8d8312e07b8da8b87205a640
Thomas Lovis <thomas.lovis@gmail.com>
2017-11-19 10:18:50 +0100

    rename table demand to demands

--- BUZZV1/database/migrations/2017_10_27_050155_create_demands_table.php
+++ BUZZV1/database/migrations/2017_10_27_050155_create_demands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDemandTable extends Migration
+class CreateDemandsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDemandTable extends Migration
      */
     public function up()
     {
-        Schema::create('demand', function (Blueprint $table) {
+        Schema::create('demands', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title')->nullable();
             $table->text('description')->nullable();
@@ -29,6 +29,6 @@ class CreateDemandTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('demand');
+        Schema::dropIfExists('demands');
     }
 }
