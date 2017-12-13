83f6c8206b51d66ddd6fe1f09504e0474644f5bd
Thomas Lovis <thomas.lovis@gmail.com>
2017-12-13 13:05:34 +0100

    url scheme

--- BUZZV1/app/Providers/AppServiceProvider.php
+++ BUZZV1/app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //\URL::forceScheme('https'):
         Schema::defaultStringLength(191);
     }
 
@@ -25,6 +25,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 }
