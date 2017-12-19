a2f562b4ab65434decf1105a1ff2d242dcc7b95e
Thomas Lovis <thomas.lovis@gmail.com>
2017-12-19 09:16:09 +0100

    merge

--- BUZZV1/resources/views/layouts/partials/headers/_slave_header.blade.php
+++ BUZZV1/resources/views/layouts/partials/headers/_slave_header.blade.php
@@ -39,7 +39,6 @@
 					</div>
 					<div class="user-menu">
 						<div class="user-name"><span><img src="{{ secure_asset('/uploads/avatars')}} Auth::user()->photo " alt=""></span>Mon Compte</div>
-						<div class="user-name"><span><img src="/uploads/avatars/{{ Auth::user()->photo }}" alt=""></span>Mon Compte</div>
 						<ul>
               <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i> Déconnexion</a>
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
